061131b0a06a712eeaebc22584298d53cb806601
Auri Munoz <amunozhe@redhat.com>
2025-03-24 12:14:01 +0100

    Disable tests on windows

--- src/test/java/org/acme/spring/web/GreetingControllerIT.java
+++ src/test/java/org/acme/spring/web/GreetingControllerIT.java
@@ -1,7 +1,10 @@
 package org.acme.spring.web;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
+@DisabledOnOs(OS.WINDOWS)
 @QuarkusIntegrationTest
 public class GreetingControllerIT extends GreetingControllerTest {
     // Execute the same tests but in packaged mode.
--- src/test/java/org/acme/spring/web/GreetingControllerTest.java
+++ src/test/java/org/acme/spring/web/GreetingControllerTest.java
@@ -2,10 +2,13 @@ package org.acme.spring.web;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+@DisabledOnOs(OS.WINDOWS)
 @QuarkusTest
 public class GreetingControllerTest {
 
